07a0b38c3100cf655dd56f08343b0bee5129ceec
Matteo Maronati <me@mattinthe.net>
2020-03-26 22:31:21 +0100

    Rm stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,10 @@ import * as React from 'react'
 import Container from '../components/Container'
 import Page from '../components/Page'
 import IndexLayout from '../layouts'
-import Entry from '../sections/entry/Entry'
-import Second from '../sections/second/Second'
 
 const IndexPage = () => (
   <IndexLayout>
     <Page>
-      <Entry />
-      <Second />
       <Container>
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
