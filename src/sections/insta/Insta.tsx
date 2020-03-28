ea32255f001912987a90c82252a6c958eecddb93
jonathan.lechner <16844326+hinterdupfinger@users.noreply.github.com>
2020-03-28 18:59:37 +0100

    add sozial links

--- src/sections/insta/Insta.tsx
+++ src/sections/insta/Insta.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react'
 import { Typography } from '@bring-n-ring/components'
 import '@bring-n-ring/components/css/ion-button.css'
-import styles from './insta.module.css'
+import * as React from 'react'
+import Sozial from '../../components/social/Sozial'
 import { Slider } from './components/Slider'
+import styles from './insta.module.css'
 
 export type InstaProps = {
   title: string
@@ -19,6 +20,7 @@ export const Insta: React.FC<InstaProps> = ({ title, body }) => {
         <Typography className={styles.body} size="p">
           {body}
         </Typography>
+        <Sozial />
       </div>
       <div className={styles.visual}>
         <Slider />
