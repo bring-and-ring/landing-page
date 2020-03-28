48171c55c80d6b80ac390bdacaf4392a29227159
Marco Karaula <marco.karaula@flixbus.com>
2020-03-28 22:12:17 +0100

    Logo (#21)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,6 +5,7 @@ import footerContent from '../../content/footer.yml'
 import { MainImage } from '../MainImage'
 import Sozial from '../social/Sozial'
 import styles from './Footer.module.css'
+import Logo from '../../content/images/logos/logo.svg'
 
 export type FooterProps = {
   btnProps?: HTMLDivElement
@@ -12,6 +13,9 @@ export type FooterProps = {
 
 const Footer: React.FC<FooterProps> = ({ btnProps = {} }) => (
   <IonFooter className={`ion-no-border ${styles.footer}`}>
+    <div className={styles.circle}>
+      <Logo className={styles.logo} />
+    </div>
     <div className={styles.grid}>
       <div className={styles.image}>
         <MainImage />
