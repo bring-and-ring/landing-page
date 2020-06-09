85df0d64cd1209b5fd62e3ee4bea73cfd9fb0e8c
Julian Giebel <j.giebel@netrocks.info>
2020-06-09 17:55:10 +0200

    Change partner grid layout Add new partner logos to grid

--- src/sections/our-mission/OurMission.tsx
+++ src/sections/our-mission/OurMission.tsx
@@ -1,12 +1,12 @@
 import { Typography } from '@bring-n-ring/components'
 import * as React from 'react'
 import styles from './OurMission.module.css'
-import { IonSlides, IonSlide, IonContent, IonImg } from '@ionic/react';
+import { IonSlides, IonSlide, IonContent, IonImg } from '@ionic/react'
 
 const slideOpts = {
   initialSlide: 1,
   speed: 400
-};
+}
 
 type Props = {
   left: {
@@ -17,8 +17,18 @@ type Props = {
   right: {
     title: string
     body: string
+    partnerIntroduction: string
     sponsorIntroduction: string
+    supporterIntroduction: string
+    partners: Array<{
+      src: string
+      alt: string
+    }>
     sponsors: Array<{
+      src: string
+      alt: string
+    }>
+    supporters: Array<{
       src: string
       alt: string
     }>
@@ -26,6 +36,7 @@ type Props = {
 }
 
 export const OurMission: React.FC<Props> = ({ left, right }) => {
+  //console.log(right.partners)
   return (
     <div className={styles.grid}>
       <div className={styles.gridItemLeft}>
@@ -44,11 +55,27 @@ export const OurMission: React.FC<Props> = ({ left, right }) => {
           <Typography className={styles.title} color="primary" size="h1">
             {right.title}
           </Typography>
-          <Typography size="p">{right.sponsorIntroduction}</Typography>
+          <Typography size="h5">{right.partnerIntroduction}</Typography>
+          <div className={styles.sponsorGrid}>
+            {right.partners.map((partner) => (
+              <div key={partner.src} data-sponsor={["netrocks", "netlight"].includes(partner.alt) ? partner.alt : ""}>
+                <img src={partner.src} alt={partner.alt} />
+              </div>
+            ))}
+          </div>
+          <Typography size="h5">{right.sponsorIntroduction}</Typography>
           <div className={styles.sponsorGrid}>
             {right.sponsors.map((sponsor) => (
               <div key={sponsor.src}>
                 <img src={sponsor.src} alt={sponsor.alt} />
+              </div>
+            ))}
+          </div>
+          <Typography size="h5">{right.supporterIntroduction}</Typography>
+          <div className={styles.sponsorGrid}>
+            {right.supporters.map((supporter) => (
+              <div key={supporter.src}>
+                <img src={supporter.src} alt={supporter.alt} />
               </div>
             ))}
           </div>
